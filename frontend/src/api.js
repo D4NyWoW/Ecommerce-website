71608fae4653fde4f0226425e8c2f667341d542f
Alex <amaritei.alex@yahoo.com>
2023-05-24 20:37:08 +0300

    mdd

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+export const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_BASE_ENDPOINT,
+});
 
-axios.interceptors.request.use(
+apiClient.interceptors.request.use(
   function (config) {
     const { origin } = new URL(config.url);
 
@@ -18,93 +21,65 @@ axios.interceptors.request.use(
 );
 
 export const fetchProductList = async ({ pageParam = 1 }) => {
-  const { data } = await axios.get(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`
-  );
+  const { data } = await apiClient.get(`/product?page=${pageParam}`);
 
   return data;
 };
 
 export const fetchProduct = async (id) => {
-  const { data } = await axios.get(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/product/${id}`
-  );
+  const { data } = await apiClient.get(`/product/${id}`);
 
   return data;
 };
 
 export const postProduct = async (input) => {
-  const { data } = await axios.post(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/product/`,
-    input
-  );
+  const { data } = await apiClient.post(`/product/`, input);
 
   return data;
 };
 
 export const fetcRegister = async (input) => {
-  const { data } = await axios.post(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`,
-    input
-  );
+  const { data } = await apiClient.post(`/auth/register`, input);
 
   return data;
 };
 
 export const fetchLogin = async (input) => {
-  const { data } = await axios.post(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/login`,
-    input
-  );
+  const { data } = await apiClient.post(`/auth/login`, input);
 
   return data;
 };
 
 export const fetchMe = async () => {
-  const { data } = await axios.get(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/me`
-  );
+  const { data } = await apiClient.get(`/auth/me`);
   return data;
 };
 
 export const fetchLogout = async () => {
-  const { data } = await axios.post(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/logout`,
-    {
-      refresh_token: localStorage.getItem("refresh-token"),
-    }
-  );
+  const { data } = await apiClient.post(`/auth/logout`, {
+    refresh_token: localStorage.getItem("refresh-token"),
+  });
   return data;
 };
 
 export const postOrder = async (input) => {
-  const { data } = await axios.post(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/order`,
-    input
-  );
+  const { data } = await apiClient.post(`/order`, input);
   return data;
 };
 
 export const fetchOrders = async () => {
-  const { data } = await axios.get(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/order`
-  );
+  const { data } = await apiClient.get(`/order`);
   return data;
 };
 
 export const deleteProduct = async (product_id) => {
-  const { data } = await axios.delete(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`
-  );
+  const { data } = await apiClient.delete(`/product/${product_id}`);
 
   return data;
 };
 
 export const updateProduct = async (input, product_id) => {
-  const { data } = await axios.put(
-    `${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`,
-    input
-  );
+  const { data } = await apiClient.put(`/product/${product_id}`, input);
 
   return data;
 };
